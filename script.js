75d0e084027ae27b82483a35e15c9a6ab006d8bf
SunBlast2255 <sunflash3412@gmail.com>
2024-12-28 15:41:34 +0300

    Added keyboard control

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 function updateCounterSpan(){
-    const span = document.getElementById("count-span");
-    const count = localStorage.getItem("count");
+    const span = document.getElementById('count-span');
+    const count = localStorage.getItem('count');
 
     if(count == null){
-        localStorage.setItem("count", "0");
+        localStorage.setItem('count', '0');
         return;
     }
 
     span.innerHTML = count;
 }
 
-document.getElementById("increase").addEventListener("click", function(){
-    const count = parseInt(localStorage.getItem("count")) + 1;
-    localStorage.setItem("count", count.toString());
+function increase(){
+    const count = parseInt(localStorage.getItem('count')) + 1;
+    localStorage.setItem('count', count.toString());
+}
+
+function decrease(){
+    const count = parseInt(localStorage.getItem('count')) - 1;
+    localStorage.setItem('count', count.toString());
+}
+
+function reset(){
+    localStorage.setItem('count', '0');
+}
+
+document.getElementById('increase').addEventListener('click', function(){
+    increase();
     updateCounterSpan();
 });
 
-document.getElementById("reset").addEventListener("click", function(){
-    localStorage.setItem("count", "0");
+document.getElementById('reset').addEventListener('click', function(){
+    reset();
     updateCounterSpan();
 });
 
-document.getElementById("decrease").addEventListener("click", function(){
-    const count = parseInt(localStorage.getItem("count")) - 1;
-    localStorage.setItem("count", count.toString());
+document.getElementById('decrease').addEventListener('click', function(){
+    decrease();
     updateCounterSpan();
+});
+
+document.addEventListener('keypress', function(e){
+
+
+    if(e.key == '0'){
+        reset();
+        updateCounterSpan();
+    }else if(e.key == '-'){
+        decrease();
+        updateCounterSpan();
+    }else if(e.key == '='){
+        increase();
+        updateCounterSpan();
+    }
 });
 
 window.onload = updateCounterSpan;
